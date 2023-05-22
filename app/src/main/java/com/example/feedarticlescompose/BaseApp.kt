83548bf6a3f4363161861a94fb8548601662ee73
Thomas Fouquert <tfouquert@gmail.com>
2023-05-22 10:10:26 +0200

    add files to packages

--- app/src/main/java/com/example/feedarticlescompose/BaseApp.kt
+++ app/src/main/java/com/example/feedarticlescompose/BaseApp.kt
@@ -1,6 +1,8 @@
 package com.example.feedarticlescompose
 
 import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BaseApp: Application() {
 }
---
+++ app/src/main/java/com/example/feedarticlescompose/ui/login/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.feedarticlescompose.ui.login
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class LoginViewModel: ViewModel() {
+}
